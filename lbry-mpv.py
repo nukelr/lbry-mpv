f449f0fa622522a6f8e3b7492c678c295e6ba9ac
Nuke <35895018+nukelr@users.noreply.github.com>
2020-11-02 19:13:48 +0100

    Update lbry-mpv.py

--- lbry-mpv.py
+++ lbry-mpv.py
@@ -3,17 +3,21 @@ import sys,json,requests,subprocess
 def main():
 	answer = requests.post("http://localhost:5279", json={"method": "get", "params": {"uri": sys.argv[1]}}).json()
 	stream_type=answer["result"]["metadata"]["stream_type"]
+	
 	if stream_type != "video":
 		sys.exit("Not a video file, exiting...")
-	description=answer["result"]["metadata"]["description"]
-	URL = answer["result"]["streaming_url"]
 	
+	URL = answer["result"]["streaming_url"]
 	print("Streaming URL:",URL )
-	print("\nVideo description:\n",description, "\n")
+	if "metadata" in answer["result"]:
+		if "description" in answer["result"]["metadata"]:
+			description=answer["result"]["metadata"]["description"]
+			print("\nVideo description:\n",description, "\n")
 	print("Playing with mpv...\n")
 	subprocess.run(["mpv", URL])
  
 
 if __name__=="__main__":
     main()
+
 
