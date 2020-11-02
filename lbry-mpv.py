c0f6c0d906fe3b6f21fd250c29ed98d66651e2e4
Nuke <35895018+nukelr@users.noreply.github.com>
2020-11-02 15:43:35 +0100

    Update lbry-mpv.py

--- lbry-mpv.py
+++ lbry-mpv.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
-
 import sys,json,requests,subprocess
 
-answer = requests.post("http://localhost:5279", json={"method": "get", "params": {"uri": sys.argv[1]}}).json()
-URL = answer["result"]["streaming_url"]
-print("Streaming URL:\n",URL )
-print("Playing with mpv...\n")
-subprocess.run(["mpv", URL])
+def main():
+	answer = requests.post("http://localhost:5279", json={"method": "get", "params": {"uri": sys.argv[1]}}).json()
+	URL = answer["result"]["streaming_url"]
+	print("Streaming URL:\n",URL )
+	print("Playing with mpv...\n")
+	subprocess.run(["mpv", URL])
  
+if __name__=="__main__":
+    main()
 
